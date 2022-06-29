42737cf606e121901d151007841b227ed81d2ecd
arnauizquierdo <arnauicat@gmail.com>
2022-06-29 19:05:31 +0200

    It can connect and get the whole code of page besides it can select the tag <a></a> and get the link (href)

--- scrapy.py
+++ scrapy.py
@@ -1,10 +1,33 @@
+# Control + K + C --> Comentar text
+# Control + K + U --> Descomentar text
+
+from cgi import print_arguments
+from cgitb import text
 import requests 
 from bs4 import BeautifulSoup
-
 import os
 
+#HOME_URL = 'https://www.english-practice.at/'
+HOME_URL = 'https://www.english-practice.at/b1/grammar/tenses/b1-tenses-index.htm'
 
-HOME_URL = 'https://www.english-practice.at/'
+try:
+    response = requests.get(HOME_URL)
+    if response.status_code == 200:
+        notice = response.content.decode('utf-8')
+        #print("hola")
 
+except ValueError as ve:
+        print(ve)
+
+soup = BeautifulSoup(response.content, 'html.parser')
+
+#print(soup.prettify())
+#print(soup.find_all('a'))
+
+allpdfs = []
+
+for link in soup.find_all('a'):
+    pdf = "https://www.english-practice.at/b1/grammar/tenses/" + link.get("href")
+    print(pdf)
 
 
