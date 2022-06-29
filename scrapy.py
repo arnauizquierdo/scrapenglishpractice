9aa9ab77fda2b58b4e913c4b9d49521c83d2a6fa
arnauizquierdo <arnauicat@gmail.com>
2022-06-29 20:24:11 +0200

    Basic utilities done. It can download all PDFs from 'Home/B1/Grammar/Tenses' 1-56

--- scrapy.py
+++ scrapy.py
@@ -7,27 +7,48 @@ import requests
 from bs4 import BeautifulSoup
 import os
 
-#HOME_URL = 'https://www.english-practice.at/'
 HOME_URL = 'https://www.english-practice.at/b1/grammar/tenses/b1-tenses-index.htm'
 
-try:
-    response = requests.get(HOME_URL)
-    if response.status_code == 200:
-        notice = response.content.decode('utf-8')
-        #print("hola")
+def main():
+    try:
+        response = requests.get(HOME_URL)
+        
+        if response.status_code == 200:
+            soup = BeautifulSoup(response.content, 'html.parser')
+            listpdf = getListOfPdfLink(soup)
 
-except ValueError as ve:
-        print(ve)
+            if len(listpdf) != 0: 
+                writePdf(listpdf)
 
-soup = BeautifulSoup(response.content, 'html.parser')
+    except ValueError as ve:
+            print(ve)
+    
 
-#print(soup.prettify())
-#print(soup.find_all('a'))
+def getListOfPdfLink(soup_content):
 
-allpdfs = []
+    listpdf = []
 
-for link in soup.find_all('a'):
-    pdf = "https://www.english-practice.at/b1/grammar/tenses/" + link.get("href")
-    print(pdf)
+    for link in soup_content.find_all('a'):
+        if link.get("href")[-4:] == ".pdf":
+            pdf = "https://www.english-practice.at/b1/grammar/tenses/" + link.get("href")
+            listpdf.append(pdf)
+    
+    return listpdf
+        
+def writePdf(listpdf):
+    
+    for i in range(len(listpdf)):
+        try:
+            response = requests.get(listpdf[i])
+            if response.status_code == 200:
+                pdf = open("pdf_list/" + listpdf[i].split("/")[6], 'wb')
+                pdf.write(response.content)
+                pdf.flush()
+                pdf.close()
 
+        except ValueError as ve:
+                print(ve)
+
+if __name__ == "__main__":
+    main()
 
